d9fec7beee2316e37ac74f7cfb893b18877c7aba
Gabriel Escame <gabriel.escame@gmail.com>
2022-11-06 18:58:35 +0100

    refactor: move SUT creation to a factory method

--- src/data/usecases/weather/remote-current-weather.spec.ts
+++ src/data/usecases/weather/remote-current-weather.spec.ts
@@ -1,13 +1,25 @@
 import RemoteCurrentWeather from './remote-current-weather'
 import { HttpGetClientSpy } from '../../test/mock-http-client'
 
+type SutTypes = {
+  sut: RemoteCurrentWeather
+  httpPostClientSpy: HttpGetClientSpy
+}
+
+const makeSut = (url: string = 'any_url'): SutTypes => {
+  const httpPostClientSpy = new HttpGetClientSpy()
+  const sut = new RemoteCurrentWeather(url, httpPostClientSpy)
+  return {
+    sut,
+    httpPostClientSpy
+  }
+}
+
 describe('RemoteCurrentWeather', () => {
   test('Should call httpClint with correct URL', async () => {
-    const url = 'any_url'
-    const httpPostClientSpy = new HttpGetClientSpy()
-    const sut = new RemoteCurrentWeather(url, httpPostClientSpy)
+    const url = 'any_other_url'
+    const { sut, httpPostClientSpy } = makeSut(url)
     await sut.current()
-
     expect(httpPostClientSpy.url).toEqual(url)
   })
 })
