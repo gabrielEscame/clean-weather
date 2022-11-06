c50f176e589ff362bd8ef2fdcedddbe9c93476d4
Gabriel Escame <gabriel.escame@gmail.com>
2022-11-06 18:50:10 +0100

    refactor: move HttpGetClientSpu to its folder

--- src/data/usecases/weather/remote-current-weather.spec.ts
+++ src/data/usecases/weather/remote-current-weather.spec.ts
@@ -1,16 +1,8 @@
 import RemoteCurrentWeather from './remote-current-weather'
-import { HttpGetClient } from 'src/data/protocols/http/http-get-client'
+import { HttpGetClientSpy } from '../../test/mock-http-client'
 
 describe('RemoteCurrentWeather', () => {
   test('Should call httpClint with correct URL', async () => {
-    class HttpGetClientSpy implements HttpGetClient {
-      url?: string
-      async get(url: string): Promise<any> {
-        this.url = url
-        return await Promise.resolve()
-      }
-    }
-
     const url = 'any_url'
     const httpPostClientSpy = new HttpGetClientSpy()
     const sut = new RemoteCurrentWeather(url, httpPostClientSpy)
