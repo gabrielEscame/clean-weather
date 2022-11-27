11e64a311b6ce839868ff6086bedd9a09b8ebd8d
Gabriel Escame <gabriel.escame@gmail.com>
2022-11-27 16:26:22 +0100

    refactor: move globl styles to main layer

--- src/presentation/Routes.tsx
+++ src/presentation/Routes.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { CurrentWeather } from './pages'
-import '@/presentation/styles/global.scss'
 
 const router = createBrowserRouter([
   {
